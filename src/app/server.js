f7132f8d9b0c67d8709e4c3a6934a20a973b64d8
hatta <hattarullah@iris.com.my>
2020-06-26 12:45:09 +0800

    v1.0.0.0
    Signed-off-by: hatta <hattarullah@iris.com.my>

--- src/app/server.js
+++ src/app/server.js
@@ -10,11 +10,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
 });
+
+app.use(express.static('./dist/package.json'));
 
 // simple route
 app.get("/", (req, res) => {
